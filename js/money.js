37dcb44302ec03ff1044113e6ddc715a69dbebbd
khadizaliza <khadizaakter19836@gmail.com>
2022-02-18 23:15:28 +0600

    second-commit

--- js/money.js
+++ js/money.js
@@ -1,7 +1,28 @@
+//
 
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    const foodInput = document.getElementById('food-input');
+    const foodAmount = foodInput.value;
+    const foodTotal = parseFloat(foodAmount)
+    const rentInput = document.getElementById('rent-input');
+    const rentAmount = rentInput.value;
+    const rentTotal = parseFloat(rentAmount)
 
-
-
+    const clothesInput = document.getElementById('clothes-input');
+    const clothesAmount = clothesInput.value;
+    const clothesTotal = parseFloat(clothesAmount)
+    const exTotal = document.getElementById('ex-total');
+    const exAmount = exTotal.innerText;
+    
+    const expensesTotal = parseFloat(exAmount);
+   
+    exTotal.innerText = foodTotal + rentTotal + clothesTotal;
+    const incomeTotal = document.getElementById('income-input');
+    const incomeAmount = incomeTotal.value;
+    const incomeTotal = document.getElementById('balance-total');
+    const balanceAmount = balanceTotal.innerText;
+    balanceTotal.innerText = exTotal.innerText + 
+})
 
 
 
